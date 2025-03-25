29c17d437b90e5ba71dcc61f2fde9b412483c2f5
f4lixmpw <lon24jone03@mpw.ac.uk>
2025-03-25 13:21:21 +0000

    sessions

--- app.py
+++ app.py
@@ -1,25 +1,64 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
+import sqlite3
+import hashlib
+
 
 app = Flask(__name__)
+app.secret_key = "random"
+
+con = sqlite3.connect("database.db")
+cur = con.cursor()
+cur.execute("")
+cur.execute( """ CREATE TABLE IF NOT EXISTS users (
+                 username VARCHAR(10) NOT NULL PRIMARY KEY,
+                 password VARCHAR(20) NOT NULL
+            )""")
+con.commit()
+con.close()
+
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "GET":
         return render_template("signup.html")
     else:
-        username = request.form["username"]
-        password = request.form["password"]
-        return "signup succsess"
+        con = sqlite3.connect("database.db")
+        cur = con.cursor()
+        hash = hashlib.sha256(request.form["password"].encode()).hexdigest()
+        cur.execute("""INSERT INTO users (username, password)
+                       VALUES (?, ?)""",
+                    (request.form["username"], hash))
+        con.commit()
+        con.close()
+        return "signup success"
     
 @app.route("/", methods=["GET", "POST"])
 def login():
     if request.method == "GET":
         return render_template("index.html")
     else:
-        if "bob" == request.form["username"] and \
-           "123" == request.form["password"]:
-            return "Hello " + request.form["username"]
+        con = sqlite3.connect("database.db")
+        cur = con.cursor()
+        # Hash the password for comparison
+        hash = hashlib.sha256(request.form["password"].encode()).hexdigest()
+        # Query the database to check if the user exists and the password matches
+        cur.execute("""SELECT * FROM users WHERE username = ? AND password = ?""",
+                    (request.form["username"], hash))
+        user = cur.fetchone()
+        con.close()
+        if user:
+            session["username"] = request.form["username"]
+            return render_template("welcome.html")
         else:
-            return "wrong password"
+            return "Invalid username or password"
+        
+@app.route("/w")
+def welcome():
+    return render_template("welcome.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("username", None)
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
